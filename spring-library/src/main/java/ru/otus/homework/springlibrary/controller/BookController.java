4e443ec73144aa45f4dd07347102be14676d4cc4
dar <ildarrr91@mail.ru>
2020-03-31 10:50:01 +0300

    уехало поле

--- spring-library/src/main/java/ru/otus/homework/springlibrary/controller/BookController.java
+++ spring-library/src/main/java/ru/otus/homework/springlibrary/controller/BookController.java
@@ -1,7 +1,12 @@
 package ru.otus.homework.springlibrary.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import ru.otus.homework.springlibrary.domain.Book;
@@ -15,12 +20,12 @@ import ru.otus.homework.springlibrary.service.BooksCrudReactiveService;
 @RequestMapping("/books")
 public class BookController {
 
+    private final BooksCrudReactiveService booksCrudService;
+
     @GetMapping
     public Flux<Book> getAllBooks() {
         return booksCrudService.getAllBooks();
     }
-
-    private final BooksCrudReactiveService booksCrudService;
 
     @GetMapping("/count")
     public Mono<Long> getBooksCount() {
